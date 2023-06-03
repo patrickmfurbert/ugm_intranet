c055b9011bcef1897a66cb4463ec3e585cb5f0e3
Patrick Furbert <patrickmfurbert@gmail.com>
2023-06-03 06:58:33 -0400

    use state to load menu items

--- src/LeftMenu.js
+++ src/LeftMenu.js
@@ -10,28 +10,64 @@ import ConstructionIcon from '@mui/icons-material/Construction';
 import BadgeIcon from '@mui/icons-material/Badge';
 import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturing';
 import Drawer from '@mui/material/Drawer';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import React from 'react';
 
 const drawerWidth = 300;
 const linkData = [
     {
         name: 'Human Resources',
+        subLinks: [
+            'Benefits',
+            'Policies',
+            'Handbook',
+        ]
     },
     {
         name: 'Quality',
+        subLinks: [
+            'Standards',
+        ]
     },
     {
         name: 'Fabrication',
+        subLinks: [
+            'Tools',
+            'Safety',
+            'Procedures',
+        ]
     },
     {
         name: 'Countertop Installations',
+        subLinks: [
+            'Procedures',
+        ]
     },
     {
         name: 'Office Tips',
+        subLinks: [
+            'FAQ',
+            'How too',
+        ]
     },
 ]
 
 function LeftMenu() {
+    const [anchorElement, setAnchorElement] = React.useState(null);
+    const [dataKeyForSubLinksMenu, setDataKeyForSubLinksMenu] = React.useState(null);
+
+    const open = Boolean(anchorElement);
+    const handleClick = (event) => {
+        setAnchorElement(event.currentTarget);
+        setDataKeyForSubLinksMenu(event.currentTarget.getAttribute('data-key'));
+        console.log('clicked');
+        console.log(event.currentTarget.getAttribute('data-key'));
+    };
+    const handleClose = () => {
+        setAnchorElement(null);
+    };
+
     return (
         <React.Fragment>
             {/* Left Drawer */}
@@ -52,7 +88,12 @@ function LeftMenu() {
                     {
                         linkData.map(item => (
                             <ListItem key={item.name} disablePadding>
-                                <ListItemButton>
+                                <ListItemButton
+                                    data-key={item.name}
+                                    aria-controls={open ? 'demo-positioned-menu' : undefined}
+                                    aria-haspopup="true"
+                                    aria-expanded={open ? 'true' : undefined}
+                                    onClick={handleClick}>
                                     <ListItemIcon>
                                         {item.name === 'Human Resources' && <Diversity1Icon />}
                                         {item.name === 'Quality' && <SquareFootIcon />}
@@ -67,6 +108,33 @@ function LeftMenu() {
                     }
                 </List>
             </Drawer>
+
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                anchorEl={anchorElement}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+            >
+
+                {
+                    Boolean(anchorElement) && (linkData.filter(data => data.name === dataKeyForSubLinksMenu)).map(data => ( data.subLinks.map( link =>
+                        <MenuItem onClick={handleClose}>{link}</MenuItem>    
+                    )))
+                }
+
+                {/* <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem> */}
+            </Menu>
         </React.Fragment>
     );
 }
